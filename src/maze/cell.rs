937902407ecb81b847ff0997339a8501745299e0
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2025-05-26 13:48:50 +0200

    add direction class

--- src/maze/cell.rs
+++ src/maze/cell.rs
@@ -1,5 +1,14 @@
 
-struct Cell {
-    walls: [bool; 4], // top, right, bottom, left
-    visited: bool,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Direction {
+    Top = 0,
+    Right = 1,
+    Bottom = 2,
+    Left = 3,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Cell {
+    pub walls: [bool; 4], // [Top, Right, Bottom, Left]
+    pub visited: bool,
 }
