d437d4f7c67b55f350576904c9dd7b5e1ea73f49
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2025-06-02 01:13:32 +0200

    refactor player class location

--- src/dungeon/mod.rs
+++ src/dungeon/mod.rs
@@ -1,34 +1,9 @@
 use crate::maze::Maze;
+use crate::player::Player;
 use pyo3::prelude::*;
 use pyo3::Py;
 use pyo3::Python;
 
-
-
-/// Represents the player in the dungeon.
-#[pyclass]
-#[derive(Clone, Debug)]
-pub struct Player {
-    #[pyo3(get, set)]
-    pub x: usize,
-    #[pyo3(get, set)]
-    pub y: usize,
-    #[pyo3(get, set)]
-    pub health: i32,
-}
-
-#[pymethods]
-impl Player {
-    #[new]
-    pub fn new(x: usize, y: usize, health: i32) -> Self {
-        Player { x, y, health }
-    }
-
-    pub fn move_to(&mut self, x: usize, y: usize) {
-        self.x = x;
-        self.y = y;
-    }
-}
 
 
 #[pyclass]
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use pyo3::prelude::*;
 pub mod maze; 
 pub mod dungeon;
+pub mod player;
 use crate::maze::Maze;
 use crate::dungeon::Dungeon;
+use crate::player::Player;
 
 #[pymodule]
 fn dungeon_core(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Maze>()?;
     m.add_class::<Dungeon>()?;
-    m.add_class::<dungeon::Player>()?;
+    m.add_class::<Player>()?;
     Ok(())
 }
